b2a342c891d89d1d58f796f6a4ef85713f11a9ca
Jash Upadhyay <jashupadhyay.work@gmail.com>
2025-01-04 08:25:35 +0530

    components update 0

---
+++ blog-layout/src/components/Intro1.js
@@ -0,0 +1,13 @@
+function Intro1()
+{
+    <div className='blog-post-intro'>
+        <h2>
+            I've Become A React Developer
+        </h2>
+    <div>
+        <p>I've completed the React Basics course and I'm happy to announce that I'm now a Junior React Developer!</p>
+        <p className='link'>Read More.....</p>
+    </div>
+    </div>
+}
+export default Intro1;
