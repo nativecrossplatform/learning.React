2956874d08cea653780e407967e78b295b14e455
Jash Upadhyay <jashupadhyay.work@gmail.com>
2025-01-04 08:49:15 +0530

    App Combining Components

--- blog-layout/src/App.js
+++ blog-layout/src/App.js
@@ -1,5 +1,21 @@
 import './App.css';
+import './components/Footer'
+import Nav from './components/Nav';
+import Promo from './components/Promo';
+import Intro1 from './components/Intro1';
+import Intro2 from './components/Intro2';
+import Intro3 from './components/Intro3';
+import Footer from './components/Footer';
 function App() {
-  
+  return (
+    <div>
+      <Nav />
+      <Promo />
+      <Intro1 />
+      <Intro2 />
+      <Intro3 />
+      <Footer />
+    </div>
+  );
 }
 export default App;
