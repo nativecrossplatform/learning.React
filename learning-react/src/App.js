092c9ce801fe1565e6b21ae74e315fe99aac9fb8
Jash Upadhyay <jashupadhyay.work@gmail.com>
2025-01-19 23:27:42 +0530

    Adding Second Button

--- learning-react/src/App.js
+++ learning-react/src/App.js
@@ -2,7 +2,10 @@ import './App.css';
 import Button from './components/Button';
 function App() {
   return (
+    <div id="container" className="container">
     <Button/>
+    <Button/>
+    </div>
   );
 }
 export default App;
