f45afed4553fc30cbe9f72acd3d5b1825d4d3437
Jash Upadhyay <jashupadhyay.work@gmail.com>
2025-01-21 09:05:27 +0530

    Adding Event Handlers To Counter App

--- learning-react/src/components/Button.js
+++ learning-react/src/components/Button.js
@@ -1,5 +1,17 @@
+import react,{ useState } from "react";
 function Button(props)
 {
+    const [clr,chng_clr] = useState("black")
+    const chn_clr = () => {
+        if(props.nm=="Increment")
+        {
+        chng_clr("green")
+        }
+        else{
+        chng_clr("red")    
+        }
+    }
+    const og_color = () => {chng_clr("black")}
     const styles = {
         border: "2px solid black",
         textAlign: "center",
@@ -7,13 +19,13 @@ function Button(props)
         borderRadius:"14%",
         padding:"30px",
         fontWeight:"bolder",
-        backgroundColor:"black",
+        backgroundColor:clr,
         color:"yellow",
         borderColor:"yellow",
         margin:"30px"
     }
     return (
-        <button id="but" className="but" onClick={props.func} style={styles}>
+        <button id="but" className="but" onClick={props.func} style={styles} onMouseOver={chn_clr} onMouseOut={og_color}>
             {props.nm}
         </button>
     );
