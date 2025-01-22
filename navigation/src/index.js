1eca148cb7315d5873d400945e029378f64ffc40
Jash Upadhyay <jashupadhyay.work@gmail.com>
2025-01-23 02:01:32 +0530

    Browser Router Import

--- navigation/src/index.js
+++ navigation/src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+import {BrowserRouter} from 'react-router-dom';
 root.render(
   <React.StrictMode>
+    <BrowserRouter>    
     <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
