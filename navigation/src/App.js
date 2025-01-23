468207012039f898d9e6980bded20bc75de4db91
Jash Upadhyay <jashupadhyay.work@gmail.com>
2025-01-23 20:47:42 +0530

    Routes

--- navigation/src/App.js
+++ navigation/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Homepage from './components/HomePage';
 import Aboutme from './components/AboutMe';
+import {Routes,Route} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
@@ -8,8 +9,8 @@ function App() {
         <a href="#" className="nav-item">Home Page</a>
         <a href="#" className="nav-item">About Me</a>
       </nav>
-      <Homepage/>
-      <Aboutme/>
+      <Route path='/' element={<Homepage/>}/>
+      <Route path='/' element={<Aboutme/>}/>
     </div>
   );
 }
