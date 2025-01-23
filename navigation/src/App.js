b1f2f167b9d086d450e6a96c65e29d8e70fa8747
Jash Upadhyay <jashupadhyay.work@gmail.com>
2025-01-23 20:41:13 +0530

    Cleanup

--- navigation/src/App.js
+++ navigation/src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import Homepage from './components/HomePage';
+import Aboutme from './components/AboutMe';
 function App() {
   return (
     <div className="App">
-      
+      <nav className="nav">
+        <a href="#" className="nav-item">Home Page</a>
+        <a href="#" className="nav-item">About Me</a>
+      </nav>
+      <Homepage/>
+      <Aboutme/>
     </div>
   );
 }
