8ed5cc28bc0a9f9856b175f57acd04d246e7dd06
Jash Upadhyay <jashupadhyay.work@gmail.com>
2025-01-23 21:01:51 +0530

    Routing Completed

--- navigation/src/App.js
+++ navigation/src/App.js
@@ -1,18 +1,19 @@
 import './App.css';
 import Homepage from './components/HomePage';
 import Aboutme from './components/AboutMe';
-import {Routes,Route} from 'react-router-dom';
+import {Routes,Route, Link} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
       <nav className="nav">
-        <a href="#" className="nav-item">Home Page</a>
-        <a href="#" className="nav-item">About Me</a>
+        <Link to='/' className="nav-tem" >Home Page</Link>
+        <Link to='/about-me' className="nav-tem" >About Me</Link>
       </nav>
+      <Routes>
       <Route path='/' element={<Homepage/>}/>
-      <Route path='/' element={<Aboutme/>}/>
+      <Route path='/about-me' element={<Aboutme/>}/>
+      </Routes>
     </div>
   );
 }
-
 export default App;
